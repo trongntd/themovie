53aba0bef4d510bc02850dbe3f00dce5c83d4427
Trong Nguyen <trongntd@gmail.com>
2018-05-08 09:10:21 +0700

    [#157302703] Add unit test for popular movies list.

---
+++ app/src/androidTest/java/com/trongntd/themovie/MainActivityAndroidTest.java
@@ -0,0 +1,26 @@
+package com.trongntd.themovie;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityAndroidTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void loadListMovie_withRepositoryHavingMovies_callShowListMovie() {
+        onView(withId(R.id.rv_list_movie)).check(new RecyclerViewItemCountAssertion(Matchers.greaterThan(1)));
+    }
+}
---
+++ app/src/test/java/com/trongntd/themovie/ListMovieUnitTest.java
@@ -0,0 +1,88 @@
+package com.trongntd.themovie;
+
+import com.trongntd.themovie.api.MovieApi;
+import com.trongntd.themovie.entity.Movie;
+import com.trongntd.themovie.entity.MovieList;
+import com.trongntd.themovie.presenter.ListMoviePresenter;
+import com.trongntd.themovie.repository.MovieRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMovieUnitTest {
+    ListMoviePresenter presenter;
+    ListMoviePresenter.View view;
+    MovieRepository movieRepository;
+
+
+    @Before
+    public void setup() {
+        movieRepository = Mockito.mock(MovieRepository.class);
+        presenter = new ListMoviePresenter(movieRepository);
+        view = Mockito.mock(ListMoviePresenter.View.class);
+        presenter.attachView(view);
+    }
+
+    @Test
+    public void loadListMovie_callShowLoading() {
+        presenter.loadPopularMovies(1);
+        Mockito.verify(view).showLoading(true);
+    }
+
+    @Test
+    public void loadListMovie_callGetPopularMovies() {
+        presenter.loadPopularMovies(1);
+        Mockito.verify(movieRepository).getPopularMovies(Mockito.eq(1), Mockito.any(MovieRepository.MovieRepositoryCallback.class));
+    }
+
+    @Test
+    public void loadListMovie_withRepositoryHavingMovies_callShowListMovie() {
+        Movie movie = new Movie();
+        final List<Movie> movies = new ArrayList<Movie>();
+        movies.add(movie);
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                MovieList movieList = new MovieList();
+                movieList.movies = movies;
+                MovieRepository.MovieRepositoryCallback<MovieList> callback = invocation.getArgument(1);
+                callback.onSuccess(movieList);
+                return null;
+            }
+        })
+                .when(movieRepository)
+                .getPopularMovies(Mockito.eq(1), Mockito.any(MovieRepository.MovieRepositoryCallback.class));;
+
+        presenter.loadPopularMovies(1);
+        Mockito.verify(view).showListMovie(movies);
+    }
+
+
+    @Test
+    public void loadListMovie_withRepositoryError_callShowError() {
+        final String error = "Error";
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                MovieRepository.MovieRepositoryCallback<MovieList> callback = invocation.getArgument(1);
+                callback.onError(Mockito.anyInt(), error);
+                return null;
+            }
+        })
+                .when(movieRepository)
+                .getPopularMovies(Mockito.eq(1), Mockito.any(MovieRepository.MovieRepositoryCallback.class));;
+
+        presenter.loadPopularMovies(1);
+        Mockito.verify(view).showError(error);
+    }
+
+
+}
